15b110cf1150f1999c5f112585e1bd1fa2d9f106
nicoliu <274250917@qq.com>
2016-06-28 10:37:33 +0800

    dateformat

--- src/learn/date/DateExample.java
+++ src/learn/date/DateExample.java
@@ -4,13 +4,17 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class DateExample {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Logger log=LoggerFactory.getLogger("DateExample.class");
 		//创建date对象
 		Date date=new Date();
-		System.out.println(date);
+		log.info(date.toString());
 		//获取时间毫秒值
 		long time=date.getTime();
 		System.out.println(time);
